b63a2433e90ca059ce4c7391f43dc4dfdbebc819
Gabriel Volk <volkdev.projects@gmail.com>
2026-03-23 18:09:56 -0300

    Atualizar o MainActivity.kt

--- app/src/main/java/com/volkdev/moodlink/MainActivity.kt
+++ app/src/main/java/com/volkdev/moodlink/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)"
 
-        // Inico do fire base
+        //inicio do fire base
         FirebaseApp.initializeApp(this)
 
         enableEdgeToEdge()
